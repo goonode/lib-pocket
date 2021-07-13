db57cc3b1065bfd20665382bc5b69bf64dc97fe2
Luigi Salvatore Palese <luigisalvatorepalese@gmail.com>
2021-07-13 13:17:43 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lpd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lib-pocket-demo';
+}
